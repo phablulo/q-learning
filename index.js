78d9ab2c306997fddf5b7da5e2d13e63ca92bd93
Phablulo <phablulo@live.com>
2018-01-17 03:19:25 -0300

    analise

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const q      = new Q({discount_factor: .8, epsilon:0.05});
 
 const actions = ['up','down','left','right'];
 let step = 0;
-let points = 1;
+let points = 0;
+let stop = false;
+let time = 1;
 (function act() {
 	// console.log('step',step);
 	const data = q.action_for_step(step++);
@@ -15,10 +17,11 @@ let points = 1;
 	const new_x = (action == 'left' ? x-1 : action == 'right' ? x+1 : x);
 	const new_y = (action == 'up' ? y-1 : action == 'down' ? y+1 : y);
 
-	const ptns  = map.pointsOn(new_x, new_y);
-	data.update(ptns);
+	const pnts  = map.pointsOn(new_x, new_y);// - step++;
+	// console.log("pnts", pnts)
+	data.update(pnts);
 
-	points += ptns;
+	points += pnts;
 	const status = map.statusOn(new_x, new_y);
 	// if (status == 'stop') return setTimeout(act, 300);
 	if (status == 'pass') {
@@ -26,11 +29,11 @@ let points = 1;
 		player[action]();
 	}
 	else if (status == 'win' || status == 'lose') {
-		console.log(status+'! Points:',points);
-		points = 1;
+		console.log(status+'! Points:',points, 'Step', step);
+		points = 0;
 		step = 0;
 		[x, y] = [1, 4];
 		player.goto(x, y);
 	}
-	return setTimeout(act, 1);
+	if (!stop) return setTimeout(act, time);
 })();
